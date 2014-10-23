14de34cd93f240ee2a36da86432503b142d40c30
RememberLenny <leonard_bogdonoff@condenast.com>
2014-10-23 19:05:10 +0530

    Initial Commit

---
+++ TouchTracker/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TouchTracker
+//
+//  Created by Leonard Bogdonoff on 10/23/14.
+//  Copyright (c) 2014 New Public Art Foundation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
